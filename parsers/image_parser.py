148f0956711ecb383acbfd3f955d46187396e83b
Sai Sanjibani Nayak <122912044+Sai-567@users.noreply.github.com>
2026-07-16 10:06:21 +0530

    Update image_parser.py

--- parsers/image_parser.py
+++ parsers/image_parser.py
@@ -2,15 +2,24 @@ import easyocr
 from PIL import Image
 import numpy as np
 
-# Initialize the OCR reader once
-reader = easyocr.Reader(['en'], gpu=False)
 
-def extract_text_from_image(image_file):
-    image = Image.open(image_file)
+reader = easyocr.Reader(
+    ['en'],
+    gpu=False
+)
+
+
+def extract_text_from_image(uploaded_file):
+
+    image = Image.open(uploaded_file)
+
     image_np = np.array(image)
 
-    results = reader.readtext(image_np)
+    result = reader.readtext(
+        image_np,
+        detail=0
+    )
 
-    text = "\n".join([result[1] for result in results])
+    text = "\n".join(result)
 
     return text
