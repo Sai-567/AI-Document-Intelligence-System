5ec94bba08c459450735e7def2641f46d4242610
Sai Sanjibani Nayak <122912044+Sai-567@users.noreply.github.com>
2026-07-16 10:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from parsers.pdf_parser import extract_text_from_pdf
 from parsers.docx_parser import extract_text_from_docx
