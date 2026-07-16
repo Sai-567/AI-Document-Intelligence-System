713adb15f3a496d6053aad6a2fdc1dc8697e16cc
Sai Sanjibani Nayak <122912044+Sai-567@users.noreply.github.com>
2026-07-16 10:18:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,3 @@ UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
 EXTRACTED_FOLDER = os.path.join(BASE_DIR, "extracted")
 MODEL_FOLDER = os.path.join(BASE_DIR, "models")
 DATA_FOLDER = os.path.join(BASE_DIR, "data")
-
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
